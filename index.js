273b84c7217bba12f3cf922c405e24e65e7c8d55
Martin Ek <mail@ekmartin.com>
2018-03-19 22:43:53 +0100

    Post events to Slack

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const querystring = require('querystring');
 const fetch = require('node-fetch');
 const dateFns = require('date-fns');
+const { promisify } = require('util');
+const { IncomingWebhook } = require('@slack/client');
 const { API_URL } = require('./config');
 
 // Fetch timeout in milliseconds:
@@ -30,12 +32,19 @@ async function callAPI(url) {
   }
 }
 
+/**
+ * Checks if the given event has a pool that activates today.
+ */
 function opensToday(event) {
   return !!event.pools.find(({ activationDate }) =>
     dateFns.isToday(activationDate)
   );
 }
 
+/**
+ * Retrieves a list of detailed events that
+ * have at least one pool opening today.
+ */
 async function retrieveEvents() {
   const date = dateFns.format(new Date(), 'YYYY-MM-DD');
   const qs = querystring.stringify({ date_after: date, page_size: 5 });
@@ -53,10 +62,31 @@ async function retrieveEvents() {
   return allEvents.filter(opensToday);
 }
 
+async function notifySlack(events) {
+  const messages = events.reduce(
+    (total, event) =>
+      total.concat(
+        event.pools.map(pool => {
+          const startTime = dateFns.format(pool.activationDate, 'H:mm');
+          return `${pool.name}: ${event.title} (åpner klokken ${startTime})`;
+        })
+      ),
+    []
+  );
+
+  const webhook = new IncomingWebhook(process.env.WEBHOOK_URL);
+  webhook.send = promisify(webhook.send);
+  await webhook.send(
+    `Arrangementer med påmelding i dag:\n${messages.join('\n')}`
+  );
+}
+
 async function run() {
   try {
     const events = await retrieveEvents();
-    console.log(events.map(e => e.pools));
+    if (events.length > 0) {
+      await notifySlack(events);
+    }
   } catch (e) {
     console.error('Error', e);
     process.exit(1);
