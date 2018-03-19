f34d8ed4bf2619af90d77bcee66025cfd7d73393
Martin Ek <mail@ekmartin.com>
2018-03-19 23:32:30 +0100

    Support both staging and production

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
-const target = process.env.NODE_ENV === 'production' ? 'lego' : 'lego-staging';
+const isProduction = process.env.NODE_ENV === 'production';
+const target = isProduction ? 'lego' : 'lego-staging';
 const BASE_URL = `https://${target}.abakus.no`;
 
 module.exports = {
   BASE_URL,
-  API_URL: `${BASE_URL}/api/v1`
+  API_URL: `${BASE_URL}/api/v1`,
+  WEBAPP_URL: isProduction
+    ? 'https://abakus.no'
+    : 'https://webapp-staging.abakus.no'
 };
